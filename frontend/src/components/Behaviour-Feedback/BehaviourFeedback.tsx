08ee68c73e5ecffedfc564b5479971d54589c236
sukhvirdi7 <sukh.virdi2@mhra.gov.uk>
2020-11-16 14:59:26 +0000

    Writing BehaviourFeedback to Mongo

--- frontend/src/components/Behaviour-Feedback/BehaviourFeedback.tsx
+++ frontend/src/components/Behaviour-Feedback/BehaviourFeedback.tsx
@@ -2,51 +2,9 @@ import React, { Component } from "react";
 import Behaviours from "../Behaviours/Behaviours"
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-
-/////////////////////////////////This shit no work bro///////////////////////////
-
-// class BehaviourFeedback extends Component {
-//   state = {
-//     behaviour: this.props
-//     }
-
-
-
-
-
-//   render() {
-//     return (
-//       <div>
-//         <p>
-//             Behaviour: {this.state.behaviour}
-//         </p>
-//       </div>
-//     );
-//   }
-// }
-
-
-////////////////////////////////////////////////////////////////////////////////
-
-interface IBehaviour {
-  behaviour: string
-}
-
-
-// const BehaviourFeedback: React.FC<IBehaviour> = (props) => {
-//     return (
-//        <div>
-//         <p>
-//             Behaviour: {props.behaviour}
-//         </p>
-//       </div>
-//     );
-// }
 const BehaviourFeedback = ({match:{params:{parseBehaviour}}}) => (
-  // props.match.params.name
   <div>
-    <h1> {parseBehaviour}</h1>
-    {/* <FakeText /> */}
+    <h1> {parseBehaviour} </h1>
   </div>
 );
 
